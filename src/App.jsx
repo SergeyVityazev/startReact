4565bf7ed304049363d3bc94c38e16364d454633
SergeyVityazev <sergivitiaz@mail.ru>
2020-04-29 20:27:16 +1000

    startReactProject

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+ 
+/*class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!!
+         </div>
+      );
+   }
+}*/
+function App() {
+   return (
+      <h1>Hell0World</h1>
+      ) 
+     }
+
+
+export default App;
