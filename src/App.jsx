f9289ff5971bfbb65db58cbb2e4507286d4121d4
SergeyVityazev <sergivitiaz@mail.ru>
2020-05-13 19:25:08 +1000

    home1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import React from 'react';
- 
-/*class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
-      );
-   }
-}*/
+import React from 'react'
+
 function App() {
-   return (
-      <h1>Hell0World</h1>
-      ) 
-     }
+  return <h1>Hell0World</h1>
+}
 
+export default App
+var x = 89
 
-export default App;
+console.log('sffdsfsdfs')
