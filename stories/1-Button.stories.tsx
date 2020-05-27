03485819d8b6cd120548d3a6ce281d08398db368
SergeyVityazev <sergivitiaz@mail.ru>
2020-05-27 19:13:18 +1000

    dev3

---
+++ stories/1-Button.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { action } from '@storybook/addon-actions'
+import { Button } from '@storybook/react/demo'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
+
+export default {
+  title: 'Button',
+  component: Button,
+}
+
+export const Text = () => (
+  <Button onClick={action('clicked')}>Hello Button</Button>
+)
+
+export const Emoji = () => (
+  <Button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool">
+      😀 😎 👍 💯
+    </span>
+  </Button>
+)
+
+Emoji.story = {
+  name: 'with emoji',
+}
+
+export const megaHeader: React.FC<{}> = () => (
+  <h1> {text('Header', 'MEGAHEADER')}</h1>
+)
