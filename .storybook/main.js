5122bb39cca917b14c9574ce6463ded93626749a
SergeyVityazev <sergivitiaz@mail.ru>
2020-05-27 19:40:01 +1000

    dev3_2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  webpackFinal: async (config) => {
-    // do mutation to the config
-
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      include: path.resolve(__dirname, '../src'),
+      use: [require.resolve('babel-loader')],
+    })
     return config
   },
 }
